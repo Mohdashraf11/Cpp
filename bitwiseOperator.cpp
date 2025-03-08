81cb14a83cef5676a10ce7a1cbf4ae4b9cdeaac7
Mohd ashraf <mohdashraf09458@gmail.com>
2025-03-09 01:08:23 +0530

    Add bitwise operator demonstration program and update VSCode settings.

---
+++ bitwiseOperator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // cout << (6&10) << endl;  ans 2 
+    // cout << (6|10) << endl; ans 14
+    // cout << (6^10) << endl; ans 12
+    // cout << (10<<2) << endl; ans 40 
+    cout << (10>>1) << endl; ans 5 
+    return 0 ;
+}
