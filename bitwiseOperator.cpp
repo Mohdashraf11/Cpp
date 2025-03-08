531031f59a8dcb87b8e44466114913ce543ba73f
Mohd ashraf <mohdashraf09458@gmail.com>
2025-03-09 01:09:02 +0530

    Remove unnecessary comment from bitwise operator demonstration

--- bitwiseOperator.cpp
+++ bitwiseOperator.cpp
@@ -6,6 +6,6 @@ int main(){
     // cout << (6|10) << endl; ans 14
     // cout << (6^10) << endl; ans 12
     // cout << (10<<2) << endl; ans 40 
-    cout << (10>>1) << endl; ans 5 
+    cout << (10>>1) << endl; 
     return 0 ;
 }
